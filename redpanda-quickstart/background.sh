cfd77e60435b661bcc13eeb968ec6004d3417eb2
JakeSCahill <jake@redpanda.com>
2023-02-13 11:00:24 +0000

    Remove background

--- redpanda-quickstart/background.sh
+++
@@ -1,7 +0,0 @@
-echo "Installing Docker Compose..."
-mkdir -p ~/.docker/cli-plugins/
-curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
-
-chmod +x ~/.docker/cli-plugins/docker-compose
-
-touch /tmp/finished
--- redpanda-quickstart/foreground.sh
+++ redpanda-quickstart/foreground.sh
@@ -1,3 +1,7 @@
 echo "Installing Docker Compose..."
-while [ ! -f /tmp/finished ]; do sleep 1; done
-echo DONE
+mkdir -p ~/.docker/cli-plugins/
+curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+
+chmod +x ~/.docker/cli-plugins/docker-compose
+
+clear
